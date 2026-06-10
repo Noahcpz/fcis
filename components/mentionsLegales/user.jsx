c615c9c4a76dfd99d207289545ed54e235f282f2
Noahcpz <noahchap2007@outlook.fr>
2026-06-10 16:13:56 +0200

    avancement site

---
+++ components/mentionsLegales/user.jsx
@@ -0,0 +1,37 @@
+import "../../styles/mentionslegales/user.css"
+export default function User(){
+    return (
+        <section className="generique">
+            <h3>
+                Propriété intelectuelle
+            </h3>
+            <p>
+                L'ensemble du contenu de ce site (textes, images, logos) 
+                est la propriété exclusive de FCIS. Toute reproduction, même partielle, 
+                est interdite sans autorisation préalable.
+            </p>
+            <h3>
+                Données personnelles
+            </h3>
+            <p>
+                Ce site ne collecte aucune donnée personnelle sans consentement. 
+                Conformément au RGPD, vous disposez d'un droit d'accès, 
+                de rectification et de suppression de vos données. 
+                Pour exercer ce droit, contactez : contact@fcis.fr
+            </p>
+            <h3>
+                Cookies
+            </h3>
+            <p>
+                Ce site n'utilise pas de cookies de tracking.
+            </p>
+            <h3>
+                Responsabilité
+            </h3>
+            <p>
+                FCIS ne saurait être tenu responsable des dommages 
+                directs ou indirects résultant de l'utilisation de ce site.
+            </p>
+        </section>
+    )
+}
